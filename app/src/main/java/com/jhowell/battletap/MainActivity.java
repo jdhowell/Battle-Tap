5145597b9e6de2120c6c5052b717a6e327a536c5
Joshua Howell <jhowell.me@gmail.com>
2015-05-18 17:02:23 -0400

    Displays increments based on touch location

--- app/src/main/java/com/jhowell/battletap/MainActivity.java
+++ app/src/main/java/com/jhowell/battletap/MainActivity.java
@@ -5,9 +5,11 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -54,6 +56,18 @@ public class MainActivity extends AppCompatActivity {
         archerCount = knightCount = calvaryCount = 0;
         updateCounter(count);
 
+        Button buttonCounter = (Button) findViewById(R.id.button_counter);
+        buttonCounter.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    int x = (int) motionEvent.getX();
+                    int y = (int) motionEvent.getY();
+                    incrementCounter(x, y);
+                }
+                return true;
+            }
+        });
 
         countAnimationThread = new Thread() {
             public void run() {
@@ -104,20 +118,22 @@ public class MainActivity extends AppCompatActivity {
     /********** | GAME METHODS | **********/
     /**********  --------------  **********/
 
-    public void incrementCounter(View v) {
+    public void incrementCounter(int x, int y) {
         updateCounter(++count);
         switch(count%3) {
             case 0:
-                firstPlusOneTextView.setY(500);
+                firstPlusOneTextView.setX(x);
+                firstPlusOneTextView.setY(y);
                 firstPlusOneTextView.startAnimation(fadeOut);
-
                 break;
             case 1:
-                secondPlusOneTextView.setY(500);
+                secondPlusOneTextView.setX(x);
+                secondPlusOneTextView.setY(y);
                 secondPlusOneTextView.startAnimation(fadeOut);
                 break;
             case 2:
-                thirdPlusOneTextView.setY(500);
+                thirdPlusOneTextView.setX(x);
+                thirdPlusOneTextView.setY(y);
                 thirdPlusOneTextView.startAnimation(fadeOut);
                 break;
         }
