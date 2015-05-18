7e4f087b5fed3763f8e5ca088b69d88f5a4ae4ba
Joshua Howell <jhowell.me@gmail.com>
2015-05-18 17:55:02 -0400

    Added comments

--- app/src/main/java/com/jhowell/battletap/MainActivity.java
+++ app/src/main/java/com/jhowell/battletap/MainActivity.java
@@ -25,12 +25,15 @@ public class MainActivity extends AppCompatActivity {
 
     // Primitives
     private int count;
-    private int archerCount, knightCount, calvaryCount;
     private boolean shouldRun = true;
 
     // Static variables
     private static final int BATTLE_REQUEST = 1;
+    
 
+    /*
+     * Initializes components, threads, and other variables
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,14 +49,13 @@ public class MainActivity extends AppCompatActivity {
         secondPlusOneTextView = (TextView) findViewById(R.id.secondPlusOneText);
         thirdPlusOneTextView = (TextView) findViewById(R.id.thirdPlusOneText);
 
-        //Initialization of animations
+        // Initialization of animations
         fadeOut = new AlphaAnimation(1.0f , 0.0f);
         fadeOut.setDuration(300);
         fadeOut.setFillBefore(true);
 
         // Implement try/catch with saving - What does this even mean josh?
         count = 0;
-        archerCount = knightCount = calvaryCount = 0;
         updateCounter(count);
 
         Button buttonCounter = (Button) findViewById(R.id.button_counter);
@@ -69,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Initialization of animation thread
         countAnimationThread = new Thread() {
             public void run() {
                 while (shouldRun) {
@@ -76,24 +79,9 @@ public class MainActivity extends AppCompatActivity {
                         Thread.sleep(2);
                         runOnUiThread(new Runnable() {
                             public void run() {
-//                                if (firstPlusOneTextView.getY() > 50) {
-                                    firstPlusOneTextView.setY(firstPlusOneTextView.getY() - 2);
-//                                    if (firstPlusOneTextView.getY() < 200) {
-//                                        firstPlusOneTextView.setVisibility(View.INVISIBLE);
-//                                    }
-//                                }
-//                                if (secondPlusOneTextView.getY() > 50) {
-                                    secondPlusOneTextView.setY(secondPlusOneTextView.getY() - 2);
-//                                    if (secondPlusOneTextView.getY() < 200) {
-//                                        secondPlusOneTextView.setVisibility(View.INVISIBLE);
-//                                    }
-//                                }
-//                                if (thirdPlusOneTextView.getY() > 50) {
-                                    thirdPlusOneTextView.setY(thirdPlusOneTextView.getY() - 2);
-//                                    if (thirdPlusOneTextView.getY() < 200) {
-//                                        thirdPlusOneTextView.setVisibility(View.INVISIBLE);
-//                                    }
-//                                }
+                                firstPlusOneTextView.setY(firstPlusOneTextView.getY() - 2);
+                                secondPlusOneTextView.setY(secondPlusOneTextView.getY() - 2);
+                                thirdPlusOneTextView.setY(thirdPlusOneTextView.getY() - 2);
                             }
                         });
 
@@ -106,6 +94,10 @@ public class MainActivity extends AppCompatActivity {
         countAnimationThread.start();
     }
 
+
+    /*
+     * Handles the results from each battle
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // If returning from battle, do the following...
@@ -114,10 +106,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /**********  --------------  **********/
-    /********** | GAME METHODS | **********/
-    /**********  --------------  **********/
 
+    /*
+     * Increments the main counter and decides which "+1 textview" to use
+     */
     public void incrementCounter(int x, int y) {
         updateCounter(++count);
         switch(count%3) {
@@ -139,6 +131,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    /*
+     * Displays the counter accordingly
+     */
     public void updateCounter(double count) {
         // If counter is too large, use E notation, else display counter. -Seems completely unnecessary..I doubt theyll click over 2billion
         if (count > 1E15)
@@ -147,7 +143,12 @@ public class MainActivity extends AppCompatActivity {
             textCounter.setText(String.format("%,d", (long) count));
     }
 
+
+    /*
+     * Alerts the user if he/she wants to begin battling
+     */
     public void startBattle(View v) {
+        // Initialization of the notification
         AlertDialog.Builder notification = new AlertDialog.Builder(this);
         notification
                 .setMessage(R.string.battle_notification)
@@ -166,35 +167,48 @@ public class MainActivity extends AppCompatActivity {
                     }
                 })
                 .create();
+
         // Display battle notification
         notification.show();
     }
 
+
+    /*
+     * Displays the amount of units for archers
+     */
     public void purchaseArcher(View v) {
         if (count >= 10) {
             String currentArcherAmount = archers.getText().toString();
             int newArcherAmount = Integer.parseInt(currentArcherAmount) + 1;
-            archers.setText(newArcherAmount + "");
+            archers.setText(String.valueOf(newArcherAmount));
             count -= 10;
             updateCounter(count);
         }
     }
 
+
+    /*
+     * Displays the amount of units for knights
+     */
     public void purchaseKnight(View v) {
         if (count >= 5) {
             String currentKnightAmount = knights.getText().toString();
             int newKnightAmount = Integer.parseInt(currentKnightAmount) + 1;
-            knights.setText(newKnightAmount + "");
+            knights.setText(String.valueOf(newKnightAmount));
             count -= 5;
             updateCounter(count);
         }
     }
 
+
+    /*
+     * Displays the amount of units for cavalry
+     */
     public void purchaseCavalry(View v) {
         if (count >= 20) {
             String currentCavalryAmount = cavalry.getText().toString();
             int newCavalryAmount = Integer.parseInt(currentCavalryAmount) + 1;
-            cavalry.setText(newCavalryAmount + "");
+            cavalry.setText(String.valueOf(newCavalryAmount));
             count -= 20;
             updateCounter(count);
         }
